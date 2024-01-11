f76d2663c38c4cbe83de57095d1c5e6c997eb6f3
SaskiaVdZ <143086816+SaskiaVdZ@users.noreply.github.com>
2024-01-11 18:36:00 +0100

    add skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,6 +12,20 @@ function Skills() {
       }}
     >
       <h1>Skills</h1>
+      <ul
+        style={{
+          listStyleType: "none",
+        }}
+      >
+        <li>
+          HTML5, CSS3, Sass, JavaScript, React, Figma, Bootstrap, Git/GitHub,
+          Visual Studios Code, Programming techniques and Agile work
+        </li>
+        <li>Fluent in English, Swedish and Dutch</li>
+        <li>Leadership, Mentoring and Education</li>
+        <li>Photoshop, Illustrator, InDesign</li>
+        <li>Google Suite</li>
+      </ul>
     </div>
   );
 }
